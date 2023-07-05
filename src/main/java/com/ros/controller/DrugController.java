39f649ffc2b7afa211b5b9fa8cee917f69269151
stazsans <2500855568@qq.com>
2023-07-05 19:59:17 +0800

    新增药品查询功能

---
+++ src/main/java/com/ros/controller/DrugController.java
@@ -0,0 +1,49 @@
+package com.ros.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ros.VO.PageInfo;
+import com.ros.domain.Drug;
+import com.ros.domain.Patient;
+import com.ros.result.Result;
+import com.ros.result.ResultUtil;
+import com.ros.service.DrugService;
+import com.ros.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/drug")
+public class DrugController {
+    @Autowired
+    private DrugService drugService;
+
+    @GetMapping("/drugList")
+    @Operation(summary = "获取所有药品")
+    public Result<List<Drug>> drugList() {
+        try {
+            List<Drug> drugList = drugService.list();
+            return ResultUtil.success(drugList);
+        } catch (Exception e) {
+            return ResultUtil.defineFail(500,"服务器内部错误");
+        }
+    }
+
+    @Operation(summary = "分页查询药品信息")
+    @GetMapping("/drugQueryPage")
+    public Result<PageInfo<Drug>> drugQueryPage(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int pageSize) {
+        try {
+            QueryWrapper<Drug> drugQueryWrapper = new QueryWrapper<>();
+            Page<Drug> drugPage = new Page<>(pageNum,pageSize,false);
+            return ResultUtil.pageSuccess(drugService.page(drugPage,drugQueryWrapper));
+        } catch (Exception e) {
+            return ResultUtil.defineFail(500,"服务器内部错误");
+        }
+    }
+}
